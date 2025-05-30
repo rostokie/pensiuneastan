9bf32511d5471d927687368752aa319d94a947eb
rostokie <robixastan@gmail.com>
2025-05-30 20:52:00 +0300

    modified:   templates/header.php

--- templates/header.php
+++ templates/header.php
@@ -27,8 +27,8 @@
     </div>
 
     <!-- Hamburger Button (mobile only) -->
-    <button id="menu-toggle" class="focus:outline-none z-50 <?= $isHome ? 'bg-white' : 'bg-white' ?> p-2">
-      <svg id="menu-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 <?= $isHome ? 'text-black' : 'text-gray-800' ?>" fill="none"
+    <button id="menu-toggle" class="focus:outline-none z-50 p-2">
+      <svg id="menu-icon" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 <?= $isHome ? 'text-white' : 'text-gray-800' ?>" fill="none"
         viewBox="0 0 24 24" stroke="currentColor">
         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
           d="M4 6h16M4 12h16M4 18h16" />
